147b8b633fb9e37f256834ef78a3586d986e9f4d
FlysonBot <flysonlin2@gmail.com>
2025-03-18 09:28:19 +0800

    chore: add loggin in game loop logic to debug

--- src/game.py
+++ src/game.py
@@ -1,12 +1,31 @@
+import logging
+import os
 from enum import Enum
 from itertools import cycle
 from time import sleep
+from venv import logger
 
 import cv2
 from PIL import ImageGrab
 
 from image_match import match_template_best_result
 from region import Region, State
+
+# Get the path to the desktop
+desktop_path = os.path.expanduser("~")
+
+# Define the log file name and create or clear it
+log_file = os.path.join(desktop_path, "poker-counter.log")
+if os.path.exists(log_file):
+    os.remove(log_file)
+
+# Set up logging configuration
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log message format
+)
+logger.info("日志文件创建成功")
 
 
 class Landlord(Enum):
@@ -58,18 +77,21 @@ class Game:
 
 
 def game_loop(interval, counter):
-
+    logger.info("开始游戏")
     while True:
         # 初始化游戏对象
         game = Game()
+        logger.info("游戏初始化完成")
 
         # 等待游戏开始
         while not game.determine_game_start(ImageGrab.grab()):
             sleep(1)
+        logger.info("游戏开始")
 
         # 初始化
         counter.reset()
         landlord = game.determine_landlord(ImageGrab.grab())
+        logger.info(f"地主是{landlord.name}")
         for _ in range(landlord.value):
             next(game.regions)
         screenshot = ImageGrab.grab()
@@ -83,17 +105,20 @@ def game_loop(interval, counter):
 
             # 如果区域处于等待状态，则等待
             if current_region.state == State.WAIT:
+                logger.info("等待")
                 sleep(interval)
                 continue
 
             # 如果区域有牌，则识别牌
             if current_region.state == State.ACTIVE:
+                logger.info("有牌")
                 cards = current_region.recognize_cards()
 
                 # 标记牌
                 for card, count in cards.items():
                     for _ in range(count):
                         counter.mark_card(card)
+                        logger.info(f"标记 {card}")
 
             # 并更新截图及当前区域
             current_region = next(game.regions)
