c39f348547538e7a848aef33a8eaad51952a66e9
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2025-03-20 10:45:47 +0800

    refactor: improve readability of backend logic

--- src/game_logic/backend_logic.py
+++ src/game_logic/backend_logic.py
@@ -2,11 +2,10 @@ from time import sleep
 from typing import NoReturn
 from itertools import cycle
 
-from regions.region_manager import RegionManager
 from classes.region import Region, State
 from config import SCREENSHOT_INTERVAL, GAME_START_INTERVAL
 from logger import logger
-from src.regions import region_manger
+from game_logic.gs import GameState
 
 
 def backend_logic(counter) -> NoReturn:
@@ -20,49 +19,49 @@ def backend_logic(counter) -> NoReturn:
 
     while True:
         # 初始化游戏对象
-        region_manger = RegionManager()
+        gs = GameState()
         logger.info("游戏初始化完成")
         counter.reset()  # 重置牌数量
 
         # 等待游戏开始
-        while not region_manger.determine_game_start(region_manger.get_screenshot()):
-            logger.debug("等待中...")
+        while not gs.is_game_started(gs.get_screenshot()):
+            logger.debug("正在等待游戏开始...")
             sleep(GAME_START_INTERVAL)
         logger.info("游戏开始")
 
         # 初始化地主
-        landlord = region_manger.determine_landlord_location(region_manger.get_screenshot())
+        landlord = gs.determine_landlord_location(gs.get_screenshot())
         logger.info(f"地主是{landlord.name}")
         region_cycle = cycle([
-            region_manger.card_regions["left"],
-            region_manger.card_regions["middle"],
-            region_manger.card_regions["right"],
+            gs.card_regions["left"],
+            gs.card_regions["middle"],
+            gs.card_regions["right"],
         ])
         for _ in range(landlord.value):
             next(region_cycle)
 
         # 获取截图
-        screenshot = region_manger.get_screenshot()
+        screenshot = gs.get_screenshot()
         current_region: Region = next(region_cycle)
         current_region.is_landlord = True  # 标记地主区域
 
         # 初始化自身
-        region_manger.card_regions["middle"].is_me = True
-        region_manger.my_cards_region.capture(region_manger.get_screenshot())
-        mark_cards(region_manger.get_my_cards())
+        gs.card_regions["middle"].is_me = True
+        gs.my_cards_region.capture(gs.get_screenshot())
+        mark_cards(gs.get_my_cards())
 
         # 实时记录
-        while not region_manger.determine_game_end(screenshot):
-            screenshot = region_manger.get_screenshot()
-            current_region.capture_region(screenshot)
-            current_region.update_region_state()
+        while not gs.is_game_ended(screenshot):
+            screenshot = gs.get_screenshot()  # 更新截图
+            current_region.capture(screenshot)  # 更新当前出牌区域截图
+            current_region.update_region_state()  # 更新当前出牌区域状态
 
-            # 如果区域处于等待状态，则等待
+            # 如果区域仍处于等待状态，则等待
             if current_region.state == State.WAIT:
                 sleep(SCREENSHOT_INTERVAL)
                 continue
 
-            # 如果区域有牌，并且不是自己，则识别牌
+            # 如果区域有牌，并且不是自己，则识别并标记牌
             if current_region.state == State.ACTIVE and not current_region.is_me:
                 mark_cards(current_region.recognize_cards())
 
